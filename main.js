82f940271d3ccfae909d890f2b49d1917456ade9
Filip Axani <filip.axani@gmail.com>
2020-12-05 20:59:23 +0100

    Simplify timer by only using seconds and no pauses

--- main.js
+++ main.js
@@ -3,20 +3,52 @@ const VueApp = {
     data() {
         return {
             repetitions: 1,
-            intervalMinutes: 1,
-            intervalPauseMinutes: 0.5,
+            intervalSeconds: 60,
             timerActive: false,
+            totalTimerCount: 0,
         }
     },
     computed: {
+        durationSeconds() {
+            return this.repetitions * this.intervalSeconds
+        },
         durationMins() {
-            return this.repetitions * (this.intervalMinutes + this.intervalPauseMinutes)
+            return this.durationSeconds / 60
         },
         activeInterval() {
             if (this.timerActive) {
+                let activeInterval = this.durationMins / this.totalTimerCount
+                console.log(activeInterval)
                 return 1
             } else {
                 return 0
+            }
+        },
+        // totalTimerCount() {
+        //     return 30
+        // }
+    },
+    methods: {
+        startTimer() {
+            console.log("Starting Timer ...")
+            this.timerActive = true
+            this.totalTimerCount = this.durationSeconds
+        },
+        resetTimer() {
+            console.log("... Timer reset.")
+            this.timerActive = false
+            this.totalTimerCount = this.durationSeconds
+        },
+    },
+    watch: {
+        totalTimerCount: {
+            handler(value) {
+                if (this.timerActive) {
+                    //this.currentCountdown = duration
+                    setTimeout(() => {
+                        this.totalTimerCount--;
+                    }, 1000);
+                }
             }
         }
     }
