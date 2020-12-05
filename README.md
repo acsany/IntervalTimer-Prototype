1cff59b2d89cedb90f8efc07bf9a89ef3d219955
Filip Axani <filip.axani@gmail.com>
2020-12-05 14:05:34 +0100

    Use VueJS 3.x

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # VueJS-HTML-Boilerplate
 
-* Used the latest VueJS version 👉  https://cdn.jsdelivr.net/npm/vue
+* Used the latest VueJS 3.x version 👉  https://unpkg.com/vue@next
 * Used the System Font stack 👉  https://css-tricks.com/snippets/css/system-font-stack/
--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-var app = new Vue({
+const VueApp = {
     el: '#app',
-    data: {
-        message: 'Hello World 🐶',
+    data() {
+        return {
+            message: 'Hello World 🐶',
+        }
+    },
+    computed: {
+        vueVersion() {
+            return Vue.version
+        }
     }
-})
+}
+
+Vue.createApp(VueApp).mount('#app')
