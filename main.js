08ba227e4144cecab1d615931de416f02a1aee3b
Filip Axani <filip.axani@gmail.com>
2020-12-06 07:56:37 +0100

    Add interval timer

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const VueApp = {
     el: '#app',
     data() {
         return {
-            repetitions: 1,
-            intervalSeconds: 60,
+            repetitions: 4,
+            intervalSeconds: 2,
             timerActive: false,
+            currentInterval: 0,
             totalTimerCount: 0,
+            intervalTimerCount: 0,
+            timerComplete: false
         }
     },
     computed: {
@@ -16,37 +19,54 @@ const VueApp = {
             return this.durationSeconds / 60
         },
         activeInterval() {
-            if (this.timerActive) {
-                let activeInterval = this.durationMins / this.totalTimerCount
-                console.log(activeInterval)
-                return 1
+            if (this.timerActive && this.totalTimerCount > 0) {
+                return this.currentInterval
             } else {
                 return 0
             }
-        },
-        // totalTimerCount() {
-        //     return 30
-        // }
+        }
     },
     methods: {
         startTimer() {
             console.log("Starting Timer ...")
+            this.timerComplete = false
             this.timerActive = true
             this.totalTimerCount = this.durationSeconds
+            this.intervalTimerCount = this.intervalSeconds
+            this.currentInterval = 1
         },
         resetTimer() {
             console.log("... Timer reset.")
+            this.timerComplete = false
             this.timerActive = false
             this.totalTimerCount = this.durationSeconds
+            this.currentInterval = 0
         },
     },
     watch: {
         totalTimerCount: {
             handler(value) {
-                if (this.timerActive) {
+                if (value > 0 && this.timerActive) {
                     //this.currentCountdown = duration
                     setTimeout(() => {
                         this.totalTimerCount--;
+                        if (this.totalTimerCount == 0) {
+                            this.resetTimer()
+                            this.timerComplete = true
+                        }
+                    }, 1000);
+                }
+            }
+        },
+        intervalTimerCount: {
+            handler(value) {
+                if (value > 0 && this.timerActive) {
+                    setTimeout(() => {
+                        this.intervalTimerCount--;
+                        if (this.intervalTimerCount == 0) {
+                            this.intervalTimerCount = this.intervalSeconds
+                            this.currentInterval++
+                        }
                     }, 1000);
                 }
             }
